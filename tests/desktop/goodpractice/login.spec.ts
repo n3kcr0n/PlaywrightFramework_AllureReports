885bc0426c01b84578858412319d3c453e3c5262
Necron <ronronmacayaon@gmail.com>
2025-10-05 21:33:02 +0800

    update the login.spec.ts page object implementation

--- tests/desktop/goodpractice/login.spec.ts
+++ tests/desktop/goodpractice/login.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
-import SauceDemoLoginPage from '../../../page_objects/sauceDemoLoginPage';
 import { config } from '../../../config/sauceDemoUserConfig';
+import PageManager from '../../../page_objects/pageManager';
 
 test('TEST01: Standar user should be able to login', async ({ page }) => {
-    const sauceDemo = new SauceDemoLoginPage(page)
+    const pm = new PageManager(page)
     await test.step('Load the application', async () => {
-        await sauceDemo.load()
+        await pm.sauceDemoLoginPage().load()
     })
     await test.step('User logged in to application', async () => {
-        sauceDemo.login(config.processEnv.username, config.processEnv.password)
+        pm.sauceDemoLoginPage().login(config.processEnv.username, config.processEnv.password)
     })
     await expect(page.getByText('Products')).toBeVisible()
     await page.context().storageState({ path: './testSessionStorage/sauceDemoLoginState.json' });
