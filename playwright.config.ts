b62bbc69b6a427b8514eb22c4ecbcbfbd547f50d
Necron <ronronmacayaon@gmail.com>
2025-10-05 22:20:15 +0800

    add outputdir on the config

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,11 +15,12 @@ export default defineConfig({
   globalSetup: "./authSessionStorage/auth.setup.ts",
   testDir: './tests',
   /* Run tests in files in parallel */
+  outputDir: 'test-results',
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
